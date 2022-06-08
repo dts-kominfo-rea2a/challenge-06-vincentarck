07a1109763c993cf0a72791afadd820fd9446e73
Vincent Laurensius <vincenthambaya@gmail.com>
2022-06-08 20:32:52 +0700

    fix: fnCallback dan bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,51 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (callBack) => {
+  let list = [];
+  fs.readFile(file1, (err, data) => {
+    if (err) return callBack(err, null);
+
+    const splitData = JSON.parse(data).message.split(" ");
+    return (!splitData)
+      ? callBack("Tidak menemukan data dari file " + file1, null)
+      : ((splitData.length < 2)
+        ? callBack(
+          "Data dari object message dari file " + file1 + " kurang dari dua kata",
+          null
+        ) : list.push(splitData[1]));
+  });
+
+  fs.readFile(file2, (err, data) => {
+    if (err) return callBack(err, null);
+
+    const splitData = JSON.parse(data)[0].message.split(" ");
+    return (!splitData)
+      ? callBack("Tidak menemukan data dari file " + file2, null)
+      : ((splitData.length < 2)
+        ? callBack(
+          "Data dari object message dari file " + file2 + " kurang dari dua kata",
+          null
+        ) : list.push(splitData[1]))
+
+  });
+  fs.readFile(file3, (err, data) => {
+    if (err) return callBack(err, null);
+
+    const splitData = JSON.parse(data)[0].data.message.split(" ");
+    if (!splitData) {
+      return callBack("Tidak menemukan data dari file " + file3, null);
+    } else if (splitData.length < 2) {
+      return callBack(
+        "Data dari object message dari file " + file3 + " kurang dari dua kata",
+        null
+      );
+    } else {
+      list.push(splitData[1]);
+    }
+    return callBack(null, list);
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
